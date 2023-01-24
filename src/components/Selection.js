935af7b85dabc2b661d117b00ef19c3b555f3300
abhi shiba <shivarajabhishek@gmail.com>
2023-01-24 18:07:20 +0530

    update

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -3,8 +3,8 @@ import React, {useState} from 'react'
 export default function Selection(props) {
     let [nextBackground, selectNextBackground] = useState({ background: "" });
   return (
-    <div>
-    <h2 className="subheading" style={nextBackground} onClick={()=> props.applyColor(selectNextBackground)}>titls</h2>
+    <div style={nextBackground} onClick={()=> props.applyColor(selectNextBackground)} >
+    <h2 className="subheading"  >Selection</h2>
     </div>
   )
 }
