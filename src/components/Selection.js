f0348a0a11bb474d9002c4f3e5c75e4217b635bd
abhi shiba <shivarajabhishek@gmail.com>
2023-01-24 18:04:18 +0530

    color pick

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,11 @@
+import React, {useState} from 'react'
+
+export default function Selection(props) {
+    let [nextBackground, selectNextBackground] = useState({ background: "" });
+  return (
+    <div>
+    <h2 className="subheading" style={nextBackground} onClick={()=> props.applyColor(selectNextBackground)}>titls</h2>
+    </div>
+  )
+}
 
